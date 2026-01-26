1a389e508d390ce046fed0f90326ab818406bf43
Seu Nome <seuemail@email.com>
2026-01-26 15:31:29 -0300

    E03 - par ou impar

---
+++ src/fases/fase1/exercicios/E03ParOuImpar.java
@@ -0,0 +1,12 @@
+package fases.fase1.exercicios;
+
+public class E03ParOuImpar {
+    public static void main(String[] args) {
+        int n = 3;
+        if  (n % 2 == 0){
+            System.out.println("Par");
+        }else {
+            System.out.println("Ímpar");
+        }
+    }
+}
