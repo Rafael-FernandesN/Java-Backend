23d93b577edf88731f26953675ea75cec6310233
Seu Nome <seuemail@email.com>
2026-01-26 15:52:24 -0300

    Rename E2 to E02 for correct ordering

--- src/fases/fase1/exercicios/E02PositivoOuNegativo.java
+++ src/fases/fase1/exercicios/E02PositivoOuNegativo.java
@@ -1,6 +1,6 @@
 package fases.fase1.exercicios;
 
-public class E2PositivoOuNegativo {
+public class E02PositivoOuNegativo {
     public static void main(String[] args) {
         int numero = 10;
         if ( numero < 0) {
