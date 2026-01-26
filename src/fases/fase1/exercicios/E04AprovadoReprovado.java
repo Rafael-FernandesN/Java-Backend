3aadccb1f0b2f75c3d73544c465c8ee248162674
Seu Nome <seuemail@email.com>
2026-01-26 15:49:31 -0300

    E05 - maior de idade

--- src/fases/fase1/exercicios/E04AprovadoReprovado.java
+++ src/fases/fase1/exercicios/E04AprovadoReprovado.java
@@ -1,4 +1,12 @@
 package fases.fase1.exercicios;
 
 public class E04AprovadoReprovado {
+    public static void main(String[] args) {
+        double nota = 6.5;
+        if (nota >= 7.0) {
+            System.out.println("Aprovado");
+        }else  {
+            System.out.println("Reprovado");
+        }
+    }
 }
---
+++ src/fases/fase1/exercicios/E05MaiorDeIdade.java
@@ -0,0 +1,12 @@
+package fases.fase1.exercicios;
+
+public class E05MaiorDeIdade {
+    public static void main(String[] args) {;
+        int idade = 17;
+        if  (idade >= 18) {
+            System.out.println("Entrada liberada");
+        }else {
+            System.out.println("Entrada negada");
+        }
+    }
+}
