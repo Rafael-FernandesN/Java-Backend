e2cc5ab00d86de5428d56df9d5e2ab7dc28484ef
Seu Nome <seuemail@email.com>
2026-01-26 15:24:42 -0300

    E02 - positivo ou negativo

---
+++ src/fases/fase1/exercicios/E02PositivoNegativo.java
@@ -0,0 +1,12 @@
+package fases.fase1.exercicios;
+
+public class E02PositivoNegativo {
+    public static void main(String[] args) {
+        int a = -3;
+        if (a > 0) {
+            System.out.println("Seu numero é positivo");
+        } else
+            System.out.println("Seu numero é negativo");
+
+    }
+}
