9b57cef1341f21e07d79cf8c60d4861f4a4cee21
Seu Nome <seuemail@email.com>
2026-01-26 16:55:33 -0300

    E11 - maior de tres numeros

--- src/fases/fase1/exercicios/E11MaiorDeTres.java
+++ src/fases/fase1/exercicios/E11MaiorDeTres.java
@@ -1,14 +1,16 @@
+
 package fases.fase1.exercicios;
 
-public class E11MaiorDeTres {
+public  class E11MaiorDeTres {
     public static void main(String[] args) {
-        int a = 10 , b = 12 , c = 0;
-        if  (a > b && b > c) {
-            System.out.println("Maior:"+ a);
-        } else if  (b > a && a > c) {
-            System.out.println("Maior:"+ b);
-        }else if  ( c > a && a > b) {
-            System.out.println("Maior:"+ c);
+        int a = 10, b = 12, c = 0;
+
+        if (a > b && a > c) {
+            System.out.println("Maior: " + a);
+        } else if (b > a && b > c) {
+            System.out.println("Maior: " + b);
+        } else {
+            System.out.println("Maior: " + c);
         }
     }
 }
