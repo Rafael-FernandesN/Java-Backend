8c70aad6eb3e13ae4d40d9fb5c2375da907e6b47
Seu Nome <seuemail@email.com>
2026-01-27 13:32:33 -0300

    E16 - maior e menor nota com array

--- src/fases/fase3/exercicios/E16_ArrayBasico.java
+++ src/fases/fase3/exercicios/E16_ArrayBasico.java
@@ -1,4 +1,20 @@
 package fases.fase3.exercicios;
 
+import java.util.Scanner;
+
 public class E16_ArrayBasico {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[]  numeros  =  new int [5];
+
+        for (int i=0;i< numeros.length;i++){
+            System.out.println("Digite um numero:");
+            numeros[i] = sc.nextInt();
+        }
+        System.out.println("Numeros Digitados:");
+        for  (int i = 0; i < numeros.length; i++){
+            System.out.print(numeros[i]);
+        }
+        sc.close();
+    }
 }
